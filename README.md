830d2bfa6f5068eac8b8f9858b57cd32089c13dc
Rebeca Divina Paula Carvalhedo <rebecadivinapc@gmail.com>
2022-07-20 22:46:41 -0300

    fim logica

--- README.md
+++ README.md
@@ -1,6 +1,35 @@
 # ler_planilha
-Objetivo: ler planilhas do exel com mais de uma aba.
-
-<h1>Tecnologias</hi>
+Objetivo: ler planilhas do exel com mais de uma aba e Retornar um Json.
+</br></br></br>
+<h1>Para Rodar o projeto</h1>
+<p>Clicke duas vezes no arquivo start.exe</p>
+</br></br>
+<h1>Tecnologias</h1>
 <p>Node.js</p>
 <p>JavaScripty</p>
+</br></br>
+<h1>Código Principal</h1>
+    var planilha = await processLineByLine();
+    var abaDaVez = planilha.getWorksheet(texto);
+    abaDaVez.eachRow(function(row, rowNumero) {
+      console.log(row);
+    });
+</br>
+<h1>Dependências</h1>
+  "dependencies": {
+    "exceljs": "^4.2.1",
+    "js-xlsx": "^0.8.22",
+    "read-excel-file": "^5.4.0",
+    "workbook": "^1.1.3",
+    "dotenv": "^8.2.0"
+  }
+</br>
+<h1>Para Criar Arquivo</h1>
+  function criarArquivoJsonPlanilha(json){
+    fs.writeFile("saidaJson.txt",JSON.stringify(json), function(erro) {
+      if(erro) {
+          throw erro;
+      }
+      console.log("Arquivo json planilha salvo!");
+    }); 
+  }
--- lerPlanilha.js
+++ lerPlanilha.js
@@ -15,58 +15,52 @@ async function init() {
 }
 init();
 
-function organizaPlanilha(planilha){
-  var ilha = [];
-  for(var k = 1; k <= 4; k++){
-    var texto = "Eventos" + k;
-    console.log(texto);
-    // var ilhaDaVez = planilha.getWorksheet(texto);
-    // ilhaDaVez.eachRow(function(row, rowNumero) {
-    //   var i = 0;
-    //   var linha = {};
-    //   row.eachCell(function(cell, cellNumero) {
-    //     if(row.number >= 7){
-    //       i++;
-    //       switch (i) {
-    //         case 1:
-    //           linha.id = cell.value;
-    //           break;
-    //         case 2:
-    //           linha.itemCatalogo = cell.value;
-    //           break;
-    //         case 3:
-    //           linha.atividade = cell.value;
-    //           break;
-    //         case 4:
-    //           linha.data = cell.value;
-    //           break;
-    //         case 5:
-    //           linha.hst = cell.value;
-    //           break;
-    //         case 6:
-    //           linha.responsavel = cell.value;
-    //           break;
-    //         default:
-    //           console.log("Coluna n'ao mapeada!");
-    //       }
-    //     }
-    //   });
-    //   if(i > 3){
-    //     ilha.push(linha);
-    //   }
-    // });
-    // var textoIlha = 'ilha' + k;
-    // tarefaPlanilha[textoIlha] = ilha;
-    // criarArquivoJsonPlanilha(tarefaPlanilha);
+  function organizaPlanilha(planilha){
+    var eventos = [];
+    var texto = "Eventos";
+    // console.log(texto);
+    var abaDaVez = planilha.getWorksheet(texto);
+    abaDaVez.eachRow(function(row, rowNumero) {
+      row.eachCell(function(cell, cellNumero) {
+        var json = {};
+        if(row.number >= 12 && cellNumero >= 3 && cellNumero <= 7){
+          // console.log(rowNumero, cellNumero);
+          // console.log("-------------");
+          // console.log(cell.value);
+          switch (cellNumero) {
+            case 3:
+              json.data = cell.value;
+              break;
+            case 4:
+              json.acao  = cell.value;
+              break;
+            case 5:
+              json.data_conclusao =  cell.value;
+              break;
+            case 6:
+              json.obs =  cell.value;
+              break;
+            case 7:
+              json.status =  cell.value;
+              break;
+            default:
+              console.log("Coluna n'ao mapeada!");
+          }
+        }
+        eventos.push(json);
+      }); 
+    });
+    console.log(eventos);
+    criarArquivoJsonPlanilha(eventos);
   }
 
   function criarArquivoJsonPlanilha(json){
-    fs.writeFile("jsonPlanilha.txt",JSON.stringify(json), function(erro) {
+    fs.writeFile("saidaJson.txt",JSON.stringify(json), function(erro) {
       if(erro) {
           throw erro;
       }
       console.log("Arquivo json planilha salvo!");
     }); 
   }
-}
+
 
